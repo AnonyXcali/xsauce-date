14a27fef5f3840e8094c8c83cf8b7ab1e6d32cae
AnonyXcali <sauravezio@gmail.com>
2024-10-17 15:27:43 +0200

    added functionality

--- src/manipulate.ts
+++ src/manipulate.ts
@@ -1,2 +1,17 @@
 import { IDateManipulator } from "./types"
 
+class DateManipulator implements IDateManipulator {
+  addDays(date: Date, days: number): Date {
+      const result = new Date(date)
+      result.setDate(date.getDate() + days)
+      return result
+  }
+
+  subtractDays(date: Date, days: number): Date {
+      const result = new Date(date)
+      result.setDate(date.getDate() - days)
+      return result
+  }
+}
+
+export default DateManipulator
